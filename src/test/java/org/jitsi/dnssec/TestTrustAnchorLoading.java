2992c0be05a16633d53f332fc29e4d05c32f87bf
Ingo Bauersachs <ingo@jitsi.org>
2013-05-26 19:30:25 +0200

    Add unit test: resolving without trust anchor must not set AD bit

--- src/test/java/org/jitsi/dnssec/TestTrustAnchorLoading.java
+++ src/test/java/org/jitsi/dnssec/TestTrustAnchorLoading.java
@@ -1,5 +1,27 @@
+/*
+ * dnssecjava - a DNSSEC validating stub resolver for Java
+ * Copyright (C) 2013 Ingo Bauersachs. All rights reserved.
+ *
+ * This file is part of dnssecjava.
+ *
+ * Dnssecjava is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Dnssecjava is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with dnssecjava.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.jitsi.dnssec;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
@@ -10,7 +32,10 @@ import org.jitsi.dnssec.validator.ValidatingResolver;
 import org.junit.Before;
 import org.junit.Test;
 import org.xbill.DNS.DClass;
+import org.xbill.DNS.Flags;
+import org.xbill.DNS.Message;
 import org.xbill.DNS.Name;
+import org.xbill.DNS.Rcode;
 import org.xbill.DNS.SimpleResolver;
 
 public class TestTrustAnchorLoading {
@@ -39,5 +64,15 @@ public class TestTrustAnchorLoading {
     public void testLoadEmptyTrustAnchors() throws IOException {
         resolver.loadTrustAnchors(getClass().getResourceAsStream( "/trust_anchors_empty"));
         assertNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
+    }
+
+    @Test
+    public void testInsecureWithEmptyTrustAnchor() throws IOException {
+        resolver.loadTrustAnchors(getClass().getResourceAsStream( "/trust_anchors_empty"));
+        assertNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
+
+        Message response = resolver.send((new TestBase(){}).createMessage("www.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
     }
 }

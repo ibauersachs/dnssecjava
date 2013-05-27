d282191ee2a28ff1660a247840051369dd809e9b
Ingo Bauersachs <ingo@jitsi.org>
2013-05-27 22:54:43 +0200

    Perform trust anchors loading tests with TestBase

--- src/test/java/org/jitsi/dnssec/TestTrustAnchorLoading.java
+++ src/test/java/org/jitsi/dnssec/TestTrustAnchorLoading.java
@@ -24,54 +24,79 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
 
-import org.jitsi.dnssec.validator.ValidatingResolver;
-import org.junit.Before;
 import org.junit.Test;
 import org.xbill.DNS.DClass;
 import org.xbill.DNS.Flags;
 import org.xbill.DNS.Message;
 import org.xbill.DNS.Name;
 import org.xbill.DNS.Rcode;
-import org.xbill.DNS.SimpleResolver;
 
-public class TestTrustAnchorLoading {
-    private ValidatingResolver resolver;
-
-    @Before
-    public void setup() throws UnknownHostException {
-        this.resolver = new ValidatingResolver(new SimpleResolver());
-    }
-
+public class TestTrustAnchorLoading extends TestBase {
     @Test
     public void testLoadRootTrustAnchors() throws IOException {
-        resolver.loadTrustAnchors(getClass().getResourceAsStream( "/trust_anchors"));
         assertNotNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
         assertNull(resolver.getTrustAnchors().find(Name.root, DClass.CH));
     }
 
     @Test
+    public void testLoadRootTrustAnchorWithDNSKEY() throws IOException {
+        resolver.getTrustAnchors().clear();
+        resolver.loadTrustAnchors(getClass().getResourceAsStream("/trust_anchors_dnskey"));
+        assertNotNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
+
+        Message response = resolver.send(createMessage("www.ingotronic.ch./A"));
+        assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
+    }
+
+    @Test
+    public void testLoadRootTrustAnchorWithInvalidDNSKEY() throws IOException {
+        resolver.getTrustAnchors().clear();
+        resolver.loadTrustAnchors(getClass().getResourceAsStream("/trust_anchors_dnskey_invalid"));
+        assertNotNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
+
+        Message response = resolver.send(createMessage("www.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+    }
+
+    @Test
+    public void testLoadRootTrustAnchorWithInvalidDS() throws IOException {
+        resolver.getTrustAnchors().clear();
+        resolver.loadTrustAnchors(getClass().getResourceAsStream("/trust_anchors_invalid"));
+        assertNotNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
+
+        Message response = resolver.send(createMessage("www.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+    }
+
+    @Test
     public void testLoadRootTrustAnchorsAlongWithGarbage() throws IOException {
-        resolver.loadTrustAnchors(getClass().getResourceAsStream( "/trust_anchors_test"));
+        resolver.getTrustAnchors().clear();
+        resolver.loadTrustAnchors(getClass().getResourceAsStream("/trust_anchors_test"));
         assertNotNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
         assertNotNull(resolver.getTrustAnchors().find(Name.root, DClass.CH));
     }
 
     @Test
     public void testLoadEmptyTrustAnchors() throws IOException {
-        resolver.loadTrustAnchors(getClass().getResourceAsStream( "/trust_anchors_empty"));
+        resolver.getTrustAnchors().clear();
+        resolver.loadTrustAnchors(getClass().getResourceAsStream("/trust_anchors_empty"));
         assertNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
     }
 
     @Test
     public void testInsecureWithEmptyTrustAnchor() throws IOException {
-        resolver.loadTrustAnchors(getClass().getResourceAsStream( "/trust_anchors_empty"));
+        resolver.getTrustAnchors().clear();
+        resolver.loadTrustAnchors(getClass().getResourceAsStream("/trust_anchors_empty"));
         assertNull(resolver.getTrustAnchors().find(Name.root, DClass.IN));
 
-        Message response = resolver.send((new TestBase(){}).createMessage("www.ingotronic.ch./A"));
+        Message response = resolver.send(createMessage("www.ingotronic.ch./A"));
         assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
         assertEquals(Rcode.NOERROR, response.getRcode());
     }

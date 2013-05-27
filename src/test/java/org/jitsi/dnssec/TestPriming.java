2244d78ae2600d81d0dd56cd3b8f99484ab0803c
Ingo Bauersachs <ingo@jitsi.org>
2013-05-27 23:15:00 +0200

    Add unit test for empty response to DS query

--- src/test/java/org/jitsi/dnssec/TestPriming.java
+++ src/test/java/org/jitsi/dnssec/TestPriming.java
@@ -1,3 +1,23 @@
+/*
+ * dnssecjava - a DNSSEC validating stub resolver for Java
+ * Copyright (C) 2013 Ingo Bauersachs. All rights reserved.
+ *
+ * This file is part of dnssecjava.
+ *
+ * Dnssecjava is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Dnssecjava is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with dnssecjava.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.jitsi.dnssec;
 
 import static org.junit.Assert.assertEquals;
@@ -17,10 +37,21 @@ import org.xbill.DNS.Type;
 
 public class TestPriming extends TestBase {
     @Test
-    public void testPrimeResponseToEmptyAnswer() throws IOException {
+    public void testDnskeyPrimeResponseWithEmptyAnswerIsBad() throws IOException {
         Message message = new Message();
         message.addRecord(Record.newRecord(Name.root, Type.DNSKEY, DClass.IN), Section.QUESTION);
         add("./DNSKEY", message);
+
+        Message response = resolver.send(createMessage("www.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+    }
+
+    @Test
+    public void testDsPrimeResponseWithEmptyAnswerIsBad() throws IOException {
+        Message message = new Message();
+        message.addRecord(Record.newRecord(Name.fromString("ch."), Type.DS, DClass.IN), Section.QUESTION);
+        add("ch./DS", message);
 
         Message response = resolver.send(createMessage("www.ingotronic.ch./A"));
         assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));

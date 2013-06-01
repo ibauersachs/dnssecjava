e9f282d18bf61ef7094fd5ea085f49d9c6e3e039
Ingo Bauersachs <ingo@jitsi.org>
2013-06-01 17:24:13 +0200

    Add (failing) unit tests for DNAMEs

---
+++ src/test/java/org/jitsi/dnssec/TestDNames.java
@@ -0,0 +1,54 @@
+/*
+ * dnssecjava - a DNSSEC validating stub resolver for Java
+ * Copyright (C) 2013 Ingo Bauersachs. All rights reserved.
+ *
+ * This file is part of dnssecjava.
+ *
+ * Dnssecjava is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Dnssecjava is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with dnssecjava.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jitsi.dnssec;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.xbill.DNS.Flags;
+import org.xbill.DNS.Message;
+import org.xbill.DNS.Rcode;
+
+public class TestDNames extends TestBase {
+    @Test
+    public void testDNameToExistingIsValid() throws IOException {
+        Message response = resolver.send(createMessage("www.alias.ingotronic.ch./A"));
+        assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
+    }
+
+    @Test
+    public void testDNameToNoDataIsValid() throws IOException {
+        Message response = resolver.send(createMessage("www.alias.ingotronic.ch./MX"));
+        assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
+    }
+
+    @Test
+    public void testDNameToNxDomainIsValid() throws IOException {
+        Message response = resolver.send(createMessage("x.alias.ingotronic.ch./A"));
+        assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
+    }
+}

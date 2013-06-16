ff1247417032e9e8ce7cc6af3fcb8a2eb2799713
Ingo Bauersachs <ingo@jitsi.org>
2013-06-17 00:20:49 +0200

    Add more DNAME unit tests

--- src/test/java/org/jitsi/dnssec/TestDNames.java
+++ src/test/java/org/jitsi/dnssec/TestDNames.java
@@ -21,14 +21,22 @@
 package org.jitsi.dnssec;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.junit.Test;
+import org.xbill.DNS.DClass;
 import org.xbill.DNS.Flags;
 import org.xbill.DNS.Message;
+import org.xbill.DNS.Name;
+import org.xbill.DNS.RRset;
 import org.xbill.DNS.Rcode;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Section;
+import org.xbill.DNS.Type;
 
 public class TestDNames extends TestBase {
     @Test
@@ -50,5 +58,61 @@ public class TestDNames extends TestBase {
         Message response = resolver.send(createMessage("x.alias.ingotronic.ch./A"));
         assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
         assertEquals(Rcode.NXDOMAIN, response.getRcode());
+    }
+
+    @Test
+    public void testDNameWithFakedCnameIsInvalid() throws IOException {
+        Message m = resolver.send(createMessage("www.alias.ingotronic.ch./A"));
+        Message message = messageFromString(m.toString().replaceAll("(.*CNAME\\s+)(.*)", "$1 www.isc.org."));
+        add("www.alias.ingotronic.ch./A", message);
+
+        Message response = resolver.send(createMessage("www.alias.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testDNameInNsecIsUnderstood_Rfc6672_5_3_4_1() throws IOException {
+        Message nsecs = resolver.send(createMessage("alias.ingotronic.ch./NS"));
+        RRset nsecSet = null;
+        for (RRset set : nsecs.getSectionRRsets(Section.AUTHORITY)) {
+            if (set.getName().equals(Name.fromString("alias.ingotronic.ch."))) {
+                nsecSet = set;
+                break;
+            }
+        }
+
+        Message message = new Message();
+        message.getHeader().setRcode(Rcode.NXDOMAIN);
+        message.addRecord(Record.newRecord(Name.fromString("www.alias.ingotronic.ch."), Type.A, DClass.IN), Section.QUESTION);
+        Iterator<Record> rrs = nsecSet.rrs();
+        while (rrs.hasNext()) {
+            message.addRecord(rrs.next(), Section.AUTHORITY);
+        }
+        Iterator<Record> sigs = nsecSet.sigs();
+        while (sigs.hasNext()) {
+            message.addRecord(sigs.next(), Section.AUTHORITY);
+        }
+
+        add("www.alias.ingotronic.ch./A", message);
+
+        Message response = resolver.send(createMessage("www.alias.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+    }
+
+    @Test
+    public void testDNameToExternal() throws IOException {
+        Message response = resolver.send(createMessage("www.isc.ingotronic.ch./A"));
+        assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
+    }
+
+    @Test
+    public void testDNameChain() throws IOException {
+        Message response = resolver.send(createMessage("www.alias.nsec3.ingotronic.ch./A"));
+        assertTrue("AD flag must be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.NOERROR, response.getRcode());
     }
 }

ca244de31c841a52b8eb66e90f74ad8f8316c929
Ingo Bauersachs <ingo@jitsi.org>
2013-07-11 22:53:09 +0200

    Add tests to ensure the validity of NSEC signatures and that wildcard synthesis was performed using the correct wildcard.

--- src/test/java/org/jitsi/dnssec/TestWildcard.java
+++ src/test/java/org/jitsi/dnssec/TestWildcard.java
@@ -27,6 +27,7 @@ import java.io.IOException;
 import org.junit.Test;
 import org.xbill.DNS.Flags;
 import org.xbill.DNS.Message;
+import org.xbill.DNS.Name;
 import org.xbill.DNS.Rcode;
 
 public class TestWildcard extends TestBase {
@@ -57,5 +58,29 @@ public class TestWildcard extends TestBase {
         assertFalse(response.getHeader().getFlag(Flags.AD));
         assertEquals(Rcode.SERVFAIL, response.getHeader().getRcode());
         assertEquals("failed.positive.wildcard_too_broad", getReason(response));
+    }
+
+    @Test
+    public void testSynthesisUsesCorrectWildcard() throws IOException {
+        Message m = resolver.send(createMessage("a.wc.ingotronic.ch./A"));
+        Message message = messageFromString(m.toString().replaceAll("a\\.wc\\.ingotronic.ch\\.", "\1.sub.wc.ingotronic.ch."));
+        add(Name.fromString("\1.sub.wc.ingotronic.ch.").toString() + "/A", message);
+
+        Message response = resolver.send(createMessage("\1.sub.wc.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+        assertEquals("failed.positive.wildcard", getReason(response));
+    }
+
+    @Test
+    public void testPositiveWithInvalidNsecSignature() throws IOException {
+        Message m = resolver.send(createMessage("a.c.ingotronic.ch./A"));
+        Message message = messageFromString(m.toString().replaceAll("(.*\\sRRSIG\\sNSEC\\s(\\d+\\s+){6}.*\\.)(.*)", "$1 YXNkZg=="));
+        add("a.c.ingotronic.ch./A", message);
+
+        Message response = resolver.send(createMessage("a.c.ingotronic.ch./A"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+        assertTrue(getReason(response).startsWith("failed.authority.positive"));
     }
 }

b675b682f8cd6c08a7b7e597dcd84bf61c762a56
Ingo Bauersachs <ingo@jitsi.org>
2013-07-28 21:48:30 +0200

    Add javadoc

--- src/main/java/org/jitsi/dnssec/validator/FindKeyState.java
+++ src/main/java/org/jitsi/dnssec/validator/FindKeyState.java
@@ -58,10 +58,36 @@ import org.xbill.DNS.Name;
  * State-object for the key-finding phase.
  */
 class FindKeyState {
+    /**
+     * The (initial) DS RRset for the following DNSKEY search and validate
+     * phase.
+     */
     SRRset dsRRset;
-    Name emptyDSName; //--> passed from DSResponse to FindKey
+
+    /**
+     * Iteratively holds the key during the search phase.
+     */
     KeyEntry keyEntry;
+
+    /**
+     * The name of the key to search. This is taken from the RRSIG's signer name
+     * or the query name if no signer name is available.
+     */
     Name signerName;
+
+    /**
+     * The query class if the key to find.
+     */
     int qclass;
+
+    /**
+     * Sets the key name being searched for when a DS response is provably not a
+     * delegation point.
+     */
+    Name emptyDSName;
+
+    /**
+     * The initial key name when the key search is started from a trust anchor.
+     */
     Name currentDSKeyName;
 }

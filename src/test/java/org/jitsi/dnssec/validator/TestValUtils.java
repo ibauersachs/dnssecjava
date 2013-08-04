2d85ac1d4475ad6cf29b70e79ea9396238c3408c
Ingo Bauersachs <ingo@jitsi.org>
2013-08-04 22:43:12 +0200

    NSEC: test NODATA when result is from delegation point

--- src/test/java/org/jitsi/dnssec/validator/TestValUtils.java
+++ src/test/java/org/jitsi/dnssec/validator/TestValUtils.java
@@ -21,13 +21,48 @@
 package org.jitsi.dnssec.validator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
+import java.io.IOException;
+
+import org.jitsi.dnssec.TestBase;
 import org.junit.Test;
+import org.xbill.DNS.Flags;
+import org.xbill.DNS.Message;
 import org.xbill.DNS.Name;
+import org.xbill.DNS.RRset;
+import org.xbill.DNS.Rcode;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Section;
 
-public class TestValUtils {
+public class TestValUtils extends TestBase {
     @Test
     public void testLongestCommonNameRootIsRoot() {
         assertEquals(Name.root, ValUtils.longestCommonName(Name.fromConstantString("example.com."), Name.fromConstantString("example.net.")));
+    }
+
+    @Test
+    public void testNoDataWhenResultIsFromDelegationPoint() throws IOException {
+        Message nsec = resolver.send(createMessage("t.ingotronic.ch./A"));
+        Record delegationNsec = null;
+        Record delegationNsecSig = null;
+        for (RRset set : nsec.getSectionRRsets(Section.AUTHORITY)) {
+            if (set.getName().toString().startsWith("sub.ingotronic.ch")) {
+                delegationNsec = set.first();
+                delegationNsecSig = (Record)set.sigs().next();
+                break;
+            }
+        }
+
+        Message m = resolver.send(createMessage("sub.ingotronic.ch./MX"));
+        Message message = messageFromString(m.toString().replaceAll("sub\\.ingotronic\\.ch\\.\\s+\\d+.*", ""));
+        message.addRecord(delegationNsec, Section.AUTHORITY);
+        message.addRecord(delegationNsecSig, Section.AUTHORITY);
+        add("sub.ingotronic.ch./MX", message);
+
+        Message response = resolver.send(createMessage("sub.ingotronic.ch./MX"));
+        assertFalse("AD flag must not be set", response.getHeader().getFlag(Flags.AD));
+        assertEquals(Rcode.SERVFAIL, response.getRcode());
+        assertEquals("failed.nodata", getReason(response));
     }
 }

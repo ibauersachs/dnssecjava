dfee426da738caa7d2ca3be8c42b4d1ed7115ed7
Ingo Bauersachs <ingo@jitsi.org>
2015-07-03 17:19:23 +0200

    Relicense to Eclipse Public License v1.0

--- src/test/java/org/jitsi/dnssec/DateMock.java
+++ src/test/java/org/jitsi/dnssec/DateMock.java
@@ -1,3 +1,13 @@
+/*
+ * dnssecjava - a DNSSEC validating stub resolver for Java
+ * Copyright (c) 2013-2015 Ingo Bauersachs
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.jitsi.dnssec;
 
 import mockit.Deencapsulation;
